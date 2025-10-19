02b101891e67005fb3cde04f9642db81d5d46ab8
Yashasvi Shukla <yashasvigot@gmail.com>
2025-10-19 23:56:40 +0530

    feat(controllers): add boilerplate for problem.controller.js

---
+++ backend/src/controllers/problem.controller.js
@@ -0,0 +1,11 @@
+export const createProblem = (req, res) => {};
+
+export const getAllProblems = (req, res) => {};
+
+export const getProblemById = (req, res) => {};
+
+export const updateProblem = (req, res) => {};
+
+export const deleteProblem = (req, res) => {};
+
+export const getAllProblemsSolvedByUser = (req, res) => {};
