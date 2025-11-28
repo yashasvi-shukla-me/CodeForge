203b169a51379f22227bd979488c02bc511e1fbc
Yashasvi Shukla <yashasvigot@gmail.com>
2025-11-28 15:55:27 +0530

    feat(playlist): working on problems playlists

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,7 @@ import problemRoutes from "./routes/problem.routes.js";
 import judge0Routes from "./routes/judge0.routes.js";
 import executionRoute from "./routes/executeCode.routes.js";
 import submissionRoutes from "./routes/submission.routes.js";
+import playlistRoutes from "./routes/playlist.routes.js";
 
 dotenv.config();
 
@@ -36,6 +37,8 @@ app.use("/api/v1/problems", problemRoutes);
 app.use("/api/v1/judge0", judge0Routes);
 app.use("/api/v1/execute-code", executionRoute);
 app.use("/api/v1/submission", submissionRoutes);
+
+app.use("/api/v1/playlist", playlistRoutes);
 
 // Start server
 const PORT = process.env.PORT || 8080;
