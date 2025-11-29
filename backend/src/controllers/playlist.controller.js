458730d0f4fb1b1948357bbfc70c901d27a908a2
Yashasvi Shukla <yashasvigot@gmail.com>
2025-11-29 10:19:30 +0530

    feat(playlist): completed playlist router

--- backend/src/controllers/playlist.controller.js
+++ backend/src/controllers/playlist.controller.js
@@ -86,8 +86,91 @@ export const getPlaylistDetails = async (req, res) => {
   }
 };
 
-export const addProblemToPlaylist = async (req, res) => {};
+export const addProblemToPlaylist = async (req, res) => {
+  const { playlistId } = req.params;
+  const { problemIds } = req.body;
 
-export const deletePlaylist = async (req, res) => {};
+  try {
+    if (!Array.isArray(problemIds) || problemIds.length === 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid problem IDs",
+      });
+    }
 
-export const removeProblemFromPlaylist = async (req, res) => {};
+    const problemsInPlaylist = await db.problemsInPlaylist.createMany({
+      data: problemIds.map((problemId) => ({
+        playlistId,
+        problemId,
+      })),
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Problems added to playlist successfully",
+      problemsInPlaylist,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+export const deletePlaylist = async (req, res) => {
+  const { playlistId } = req.params;
+
+  try {
+    const deletedPlaylist = await db.playlist.delete({
+      where: {
+        id: playlistId,
+      },
+    });
+    res.status(200).json({
+      success: true,
+      message: "Playlist deleted successfully",
+      deletedPlaylist,
+    });
+  } catch (error) {
+    if (error.code === "P2025") {
+      return res.status(404).json({
+        success: false,
+        message: "Playlist not found",
+      });
+    }
+  }
+};
+
+export const removeProblemFromPlaylist = async (req, res) => {
+  const { playlistId } = req.params;
+  const { problemIds } = req.body;
+
+  try {
+    if (!Array.isArray(problemIds) || problemIds.length === 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid problem IDs",
+      });
+    }
+
+    const deletedProblems = await db.problemsInPlaylist.deleteMany({
+      where: {
+        playlistId,
+        problemId: {
+          in: problemIds,
+        },
+      },
+    });
+    res.status(200).json({
+      success: true,
+      message: "Problems removed from playlist successfully",
+      deletedProblems,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
--- backend/src/routes/playlist.routes.js
+++ backend/src/routes/playlist.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { authMiddleware } from "../middleware/auth.middleware";
+import { authMiddleware } from "../middleware/auth.middleware.js";
 import {
   getAllListDetails,
   getPlaylistDetails,
