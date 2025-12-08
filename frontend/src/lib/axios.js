0be898c0fc4fb0e7bf0c485e77f302e8af65a0a5
Yashasvi Shukla <yashasvigot@gmail.com>
2025-12-08 16:36:43 +0530

    using axios for frontend

---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:8000/api/v1"
+      : "/api/v1",
+  withCredentials: true,
+});
