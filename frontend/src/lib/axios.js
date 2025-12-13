9e4f602c272e8313747336b80b57454b31c51377
Yashasvi Shukla <yashasvigot@gmail.com>
2025-12-13 21:26:14 +0530

    changed port number in axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "http://localhost:8000/api/v1"
+      ? "http://localhost:8080/api/v1"
       : "/api/v1",
   withCredentials: true,
 });
