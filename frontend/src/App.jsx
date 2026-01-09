41f51c892a45a7186d98c3247a31721a13a6cc11
Yashasvi Shukla <yashasvigot@gmail.com>
2026-01-09 15:36:35 +0530

    page: problemPage created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,64 +1,66 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { useEffect } from 'react';
-import { Toaster } from 'react-hot-toast';
-import HomePage from './page/HomePage.jsx';
-import LoginPage from './page/LoginPage.jsx';
-import SignUpPage from './page/SignUpPage.jsx';
-import { useAuthStore } from './store/useAuthStore.js';
-import {Loader } from 'lucide-react';
-import Layout from './layout/layout.jsx';
-import AdminRoute from './components/AdminRoute.jsx';
-import AddProblem from './page/AddProblem.jsx';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { Toaster } from "react-hot-toast";
+import HomePage from "./page/HomePage.jsx";
+import LoginPage from "./page/LoginPage.jsx";
+import SignUpPage from "./page/SignUpPage.jsx";
+import { useAuthStore } from "./store/useAuthStore.js";
+import { Loader } from "lucide-react";
+import Layout from "./layout/layout.jsx";
+import AdminRoute from "./components/AdminRoute.jsx";
+import AddProblem from "./page/AddProblem.jsx";
+import ProblemPage from "./page/ProblemPage.jsx";
 
 function App() {
-
-  const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
-  if(isCheckingAuth && !authUser) {
+  if (isCheckingAuth && !authUser) {
     return (
-      <div className='flex items-center justify-center h-screen'>
-        <Loader className='size-10 animate-spin' />
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
       </div>
-    )
+    );
   }
 
-
   return (
-    <div className='flex flex-col items-center justify-start'>
-    <Toaster />
-    <Routes>
+    <div className="flex flex-col items-center justify-start">
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route
+            index
+            element={authUser ? <HomePage /> : <Navigate to="/login" />}
+          />
+        </Route>
 
-      <Route path="/" element={<Layout />}>
-      <Route 
-      index
-      element={authUser ? <HomePage /> : <Navigate to='/login' /> }
-      />
-
-      </Route>
-
-      <Route
-      path='/login'
-      element={!authUser ? <LoginPage/> : <Navigate to='/' />}
-      />
-      <Route 
-      path='/signup'
-      element={!authUser ? <SignUpPage /> : <Navigate to='/' />}
-      />
-
-      <Route element={<AdminRoute />}>
-        <Route path="/add-problem" element={authUser ? <AddProblem/> : <Navigate to="/"/>}
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
         />
-      </Route>
-      <Route>
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
 
-      </Route>
-    </Routes>
+        <Route
+          path="/problem/:id"
+          element={authUser ? <ProblemPage /> : <Navigate to="/login" />}
+        />
+
+        <Route element={<AdminRoute />}>
+          <Route
+            path="/add-problem"
+            element={authUser ? <AddProblem /> : <Navigate to="/" />}
+          />
+        </Route>
+        <Route></Route>
+      </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
