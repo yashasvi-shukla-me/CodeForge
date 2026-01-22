b0dcd08c018015ff45f9ccf16d700a4eee6c5355
Yashasvi Shukla <yashasvigot@gmail.com>
2026-01-22 19:15:36 +0530

    allow vercel frontend in CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,18 +25,13 @@ app.get("/health", (req, res) => {
 app.use(express.json());
 app.use(cookieParser());
 
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_URL,
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   }),
-// );
-
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:5173"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "https://codeforge-theta.vercel.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
