f456ed78d344f7ad873714f03577d5cf7f523ed2
Yashasvi Shukla <yashasvigot@gmail.com>
2026-01-22 18:33:08 +0530

    add frontend production API config

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:8080/api/v1"
-      : "/api/v1",
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/v1`,
   withCredentials: true,
 });
